0a8e37dbd0f429cae40920fc6e00d14e78037631
서영준 <seoyoungjun88@gmail.com>
2019-10-11 23:49:04 +0900

    2019.10.11 Dean - Qt Designer ui

--- pyside2/versionChk.py
+++ pyside2/versionChk.py
@@ -1,9 +1,7 @@
 import cv2
 import PySide2
-import PyQt5.QtCore
 
-PyQt5.QtCore.qVersion()
+print(PySide2.__version__)
 
-print(Pyside2.__version__)
 
 
---
+++ ui/MainView.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainView.ui',
+# licensing of 'MainView.ui' applies.
+#
+# Created: Fri Oct 11 20:57:24 2019
+#      by: pyside2-uic  running on PySide2 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(670, 10, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(670, 40, 113, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("MainWindow", "Button1", None, -1))
+        self.pushButton_2.setText(QtWidgets.QApplication.translate("MainWindow", "Button2", None, -1))
+
